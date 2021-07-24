ad3d317c44e1a601db145f0e6d1ee14d6b4e9dd1
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-07-24 14:16:54 +0700

    :hammer: update color script

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
+const colors = ["green", "red", "yellow", "blue", "beige", "black", "brown", "chocolate", "coral", "ivory"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
